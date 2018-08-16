89ed8f9c43b633473a7ff8ec3852a3bf680af0c7
yousky <y1o2u3@gmail.com>
2018-08-16 15:20:53 +0900

    APIExceptionHandler 추가.

---
+++ BWYouCore.Web.MVC/Etc/APIExceptionHandler.cs
@@ -0,0 +1,35 @@
+﻿using BWYouCore.Web.MVC.ViewModels;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BWYouCore.Web.MVC.Etc
+{
+    public class APIExceptionHandler
+    {
+        public async Task Invoke(HttpContext context)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var ex = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+            if (ex == null) return;
+
+            var vm = new ErrorResultViewModel(context.Response.StatusCode, ex);
+
+            context.Response.ContentType = "application/json";
+
+            using (var writer = new StreamWriter(context.Response.Body))
+            {
+                new JsonSerializer().Serialize(writer, vm);
+                await writer.FlushAsync().ConfigureAwait(false);
+            }
+        }
+    }
+}
