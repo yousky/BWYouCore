8b914c2d8985a73c50a1153821cbdc0b02c92410
yousky <y1o2u3@gmail.com>
2020-09-24 10:21:38 +0900

    ErrorResultVMException 클래스 추가.

---
+++ BWYouCore.Web.MVC/Exceptions/ErrorResultVMException.cs
@@ -0,0 +1,27 @@
+﻿using BWYouCore.Web.MVC.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BWYouCore.Web.MVC.Exceptions
+{
+    public class ErrorResultVMException : Exception
+    {
+        public ErrorResultViewModel ErrorResultViewModel { get; set; }
+
+        public ErrorResultVMException() : base()
+        {
+
+        }
+        public ErrorResultVMException(string message)
+            : base(message)
+        {
+
+        }
+        public ErrorResultVMException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+
+        }
+    }
+}
